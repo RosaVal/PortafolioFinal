4dea999c102efcd5af46351cbb81e79304f2e6ba
Rosa <valenzuela.rosa.rosa@gmail.com>
2020-11-13 22:26:07 -0300

    commit 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,12 @@
 
+$(function () {
+    $("#enviameunmail").click(
+    function() {
+        alert("El Correo fue enviado correctamente")
+    }
+    ) 
 
-
-
-*//
-
-$(function(){
-    $("a").click(function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-
-            var gato = this.hash;
-            $('html, body').animate({
-                scrollTop: $(gato).offset().top
-                }, 800, function(){
-                    window.location.hash = gato;
-                });
-            }
-        });
-    });
-
-
-    $(function () {
-        $('[data-toggle="popover"]').popover()
+    $('.card-title').on("click", function(){
+    $('.card-text').toggle()
     })
+})
