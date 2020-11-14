e719d030660f04d5372adf7ebba47a0ec1e1e748
Rosa <valenzuela.rosa.rosa@gmail.com>
2020-11-14 18:51:12 -0300

    commit 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ $(function () {
     $("#enviameunmail").click(
     function() {
         alert("El Correo fue enviado correctamente")
-    }
-    ) 
+    }) 
 
     $('.card-title').on("click", function(){
     $('.card-text').toggle()
